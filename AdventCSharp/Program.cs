0fe3e8d3eb4ec14e31d007342043122ad389c542
Anthony Peterson <apeterson@burnettefoods.com>
2018-12-09 20:25:22 -0500

    Added tester code for closed form finding

--- AdventCSharp/Program.cs
+++ AdventCSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventCSharp
 {
@@ -13,14 +14,34 @@ namespace AdventCSharp
         [STAThread]
         static void Main(string[] args)
         {
-            // 424 players; last marble is worth 7114400 points
-            long bestScore = CircularLinkedList.playGame(424, 7114400L);
-            Console.WriteLine(bestScore);
+            //// 424 players; last marble is worth 7114400 points
+            //long bestScore = CircularLinkedList.playGame(424, 7114400L);
+            //Console.WriteLine(bestScore);
 
-            // 10 players; last marble is worth 1618 points
-            long bestScoreTest = CircularLinkedList.playGame(10, 1618L);
+            //// 10 players; last marble is worth 1618 points
+            //long bestScoreTest = CircularLinkedList.playGame(10, 1618L);
 
-            Console.WriteLine(bestScoreTest);
+            //Console.WriteLine(bestScoreTest);
+
+            long res;
+
+            var sw = File.CreateText("tester.csv");
+
+            for(long i = 1; i <= 100000; i++)
+            {
+                res = CircularLinkedList.playGame(2, i);
+
+                sw.WriteLine("{0},{1}", i, res);
+                
+                if(i % 1000 == 0)
+                {
+                    Console.WriteLine("{0},{1}", i, res);
+
+                }
+            }
+
+            sw.Close();
+
         }
     }
 }
