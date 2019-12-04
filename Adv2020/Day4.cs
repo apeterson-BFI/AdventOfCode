744bd040b5a1d14387e03ad14933ed7dff3fb082
Anthony Peterson <apeterson@burnettefoods.com>
2019-12-04 05:04:23 -0500

    temp update

---
+++ Adv2020/Day4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adv2020
+{
+    public class Day4
+    {
+        public long getPart1Answer()
+        {
+            string line = DayInput.readDayLines(4)[0];
+
+            List<long> bounds = line.Split('-').Select(v => Int64.Parse(v)).ToList();
+
+            // How many numbers between LB and UP have this property
+            // Left to Right, The digits never decrease
+            // There is at least one pair of the same digits.
+
+            // Ruling out candidates: if left is greater than right, no go.
+            // left = right : good candidate
+            // 
+            // six digit numbers
+            // 
+        }
+    }
+}
