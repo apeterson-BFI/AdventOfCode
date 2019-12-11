af82cc24d732cc249eeaee6c23cd95606b6ef4bb
Anthony Peterson <apeterson@burnettefoods.com>
2019-12-11 15:32:46 -0500

    playing with reorg

---
+++ Adv2015/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Adv2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doDay1Display();
+            string line = Console.ReadLine();
+        }
+
+        public static void doDay1Display()
+        {
+
+        }
+    }
+}
---
+++ AdventCommon/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdventCommon
+{
+    public class Class1
+    {
+    }
+}
